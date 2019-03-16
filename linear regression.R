81dad8713050ab0174dea004b16ad5d2d0f9b0da
Harishmohanan18 <harishmohanan18@gmail.com>
2019-03-16 14:07:07 +0530

    r

---
+++ linear regression.R
@@ -0,0 +1,23 @@
+women
+names(women)
+str(women)
+model1 = lm(weight~height, data=women)
+summary(model1)
+
+y=mx+c
+height=c(63)
+weight<- 3.45*height-87
+weight
+ndata1 = data.frame(height=c(63,66,68))
+p1=predict(model1, newdata=ndata1, type='response')
+cbind(ndata1,p1)
+range(women$weight)
+
+head(mtcars)
+str(mtcars)
+
+model2 = lm(mpg~wt+hp+disp,data=mtcars)
+summary(model2)
+
+model3 =lm(mpg~ wt+hp,data=mtcars)
+summary(model3)
